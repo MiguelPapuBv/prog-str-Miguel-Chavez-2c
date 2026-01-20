e8a95b2c2401b7eda54b04a68105d511b6897f06
Miguel Chavez <20253ds075@utez.edu.mx>
2026-01-19 23:05:08 -0600

    Resolucion de conflicto IVA DESCUENTO

--- GitConflictoChavezMiguel/src/Main.java
+++ GitConflictoChavezMiguel/src/Main.java
@@ -1,39 +1,43 @@
 import java.util.Scanner;
 public class Main {
-<<<<<<< HEAD
+
     public static double DESCUENTO = 0.10;
     public static double UMBRAL_DESCUENTO = 1000.0;
-=======
     public static double IVA = 0.16;
->>>>>>> feature/iva
 
-    double subtotal = pedirDouble(scanner, "Subtotal: ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double subtotal = pedirDouble(scanner, "Subtotal: ");
 
 
-    double total = calcularIVA(subtotal);
+        double total;
+        total = calcularIVA(subtotal);
 
 
-    double  total = condicionDescuento(total, subtotal);
+        total = condicionDescuento(total, subtotal);
 
         System.out.printf("Total a pagar: %.2f%n", total);
+    }
 
     public static double pedirDouble(Scanner scanner, String mensaje) {
         System.out.print(mensaje);
         return scanner.nextDouble();
     }
-<<<<<<< HEAD
-    public static double condicionDescuento(double subtotal, double total){
-        total = subtotal;
-        if(subtotal > UMBRAL_DESCUENTO) return total = total - (total * DESCUENTO);
-        else
-        return 0;
-    }
-=======
 
-    public static double calcularIVA( double subtotal){
+    public static double condicionDescuento(double total, double subtotal) {
+        if (subtotal > UMBRAL_DESCUENTO) {
+            // Aplicamos el descuento al total que ya tiene IVA
+            return total - (total * DESCUENTO);
+        } else {
+            return total;
+
+
+        }
+    }
+
+    public static double calcularIVA(double subtotal) {
 
         return subtotal + (subtotal * IVA);
     }
-
->>>>>>> feature/iva
 }
